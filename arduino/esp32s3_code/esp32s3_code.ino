1e3b49b1fa23da3cd6bb24ac6d61c4011231a98d
vivekmaddineni <vivekmaddineni@gmail.com>
2026-03-23 09:54:55 -0500

    suchir lock in again plz

--- arduino/esp32s3_code/esp32s3_code.ino
+++ arduino/esp32s3_code/esp32s3_code.ino
@@ -6,6 +6,10 @@
 const char* ssid = "LifeStep";
 const char* password = "password";
 
+const int pinA1 = A1; 
+const int pinA2 = A2;
+const int pinA3 = A3;
+
 MPU6050 mpu(Wire);
 WebSocketsServer webSocket = WebSocketsServer(81);
 unsigned long timer = 0;
@@ -17,6 +21,8 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+
+  analogReadResolution(12); // Sets range 0-4095
 
   // 1. Setup WiFi AP
   WiFi.softAP(ssid, password);
@@ -41,6 +47,17 @@ void loop() {
   mpu.update();
   webSocket.loop(); 
 
+   // Read raw values
+  int val1 = analogRead(pinA1);
+  int val2 = analogRead(pinA2);
+  int val3 = analogRead(pinA3);
+
+  // Map 0-4095 to 0-100%
+  // Using float for precision, or long for speed
+  float pct1 = (val1 / 4095.0) * 100.0;
+  float pct2 = (val2 / 4095.0) * 100.0;
+  float pct3 = (val3 / 4095.0) * 100.0 * 4;
+
   if (millis() - timer > 50) {
     // Only broadcast if at least one client is connected
     if (WiFi.softAPgetStationNum() > 0) {
@@ -57,11 +74,11 @@ void loop() {
       
       // Pressure Object
       json += "\"pressure\":{";
-      json += "\"arch\":" + String(arch) + ",";
-      json += "\"ball\":" + String(ball) + ",";
-      json += "\"heel_left\":" + String(h_left) + ",";
-      json += "\"heel_right\":" + String(h_right) + ",";
-      json += "\"toe\":" + String(toe);
+      json += "\"arch\":" + String(pct2) + ",";
+      json += "\"ball\":" + String(pct1) + ",";
+      json += "\"heel_left\":" + String(pct3) + ",";
+      json += "\"heel_right\":" + String(pct3) + ",";
+      json += "\"toe\":" + String(pct1);
       json += "},";
 
       // IMU Object
